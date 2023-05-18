b90e25025a054325f1909a13f89706bdfcd3151a
KingParry <kingparry@yahoo.com>
2023-05-18 13:55:47 -0700

    simple shell

---
+++ _shell_loop.c
@@ -0,0 +1,153 @@
+#include "shell.h"
+
+/**
+ * hsh - main shell loop
+ * @info: input
+ * @av: input
+ * Return: Always 0
+ */
+int hsh(info_t *info, char **av)
+{
+	ssize_t c = 0;
+	int g = 0;
+
+	while (c != -1 && g != -2)
+	{
+		clear_info(info);
+		if (interactive(info))
+			_puts("$ ");
+		_eputchar(BUF_FLUSH);
+		c = get_input(info);
+		if (c != -1)
+		{
+			set_info(info, av);
+			g = find_builtin(info);
+			if (g == -1)
+				find_cmd(info);
+		}
+		else if (interactive(info))
+			_putchar('\n');
+		free_info(info, 0);
+	}
+	write_history(info);
+	free_info(info, 1);
+	if (!interactive(info) && info->status)
+		exit(info->status);
+	if (g == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (g);
+}
+
+/**
+ * find_builtin - main function
+ * @info: input
+ * Return: Always 0
+ */
+
+int find_builtin(info_t *info)
+{
+	int y, bt = -1;
+	builtin_table builtintbl[] = {
+		{"exit", _myexit},
+		{"env", _myenv},
+		{"help", _myhelp},
+		{"history", _myhistory},
+		{"setenv", _mysetenv},
+		{"unsetenv", _myunsetenv},
+		{"cd", _mycd},
+		{"alias", _myalias},
+		{NULL, NULL}
+	};
+
+	for (y = 0; builtintbl[y].type; y++)
+		if (_strcmp(info->argv[0], builtintbl[y].type) == 0)
+		{
+			info->line_count++;
+			bt = builtintbl[y].func(info);
+			break;
+		}
+	return (bt);
+}
+
+/**
+ * find_cmd - program begins
+ * @info: input
+ * Return:  void
+ */
+void find_cmd(info_t *info)
+{
+	char *path = NULL;
+	int r, v;
+
+	info->path = info->argv[0];
+	if (info->linecount_flag == 1)
+	{
+		info->line_count++;
+		info->linecount_flag = 0;
+	}
+	for (r = 0, v = 0; info->arg[r]; r++)
+		if (!is_delim(info->arg[r], " \t\n"))
+			v++;
+	if (!v)
+		return;
+
+	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
+	if (path)
+	{
+		info->path = path;
+		fork_cmd(info);
+	}
+	else
+	{
+		if ((interactive(info) || _getenv(info, "PATH=")
+					|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+			fork_cmd(info);
+		else if (*(info->arg) != '\n')
+		{
+			info->status = 127;
+			print_error(info, "not found\n");
+		}
+	}
+}
+
+/**
+ * fork_cmd - main entry
+ * @info: input
+ * Return: void
+ */
+
+void fork_cmd(info_t *info)
+{
+	pid_t ch;
+
+	ch = fork();
+	if (ch == -1)
+	{
+		perror("Error:");
+		return;
+	}
+	if (ch == 0)
+	{
+		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		{
+			free_info(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_error(info, "Permission denied\n");
+		}
+	}
+}
---
+++ _vars.c
@@ -0,0 +1,155 @@
+#include "shell.h"
+
+/**
+ * is_chain - main function
+ * @info: input one
+ * @buf: input two
+ * @p: input
+ * Return: Always 0
+ */
+int is_chain(info_t *info, char *buf, size_t *p)
+{
+	size_t f = *p; /*intialization*/
+
+	if (buf[f] == '|' && buf[f + 1] == '|')
+	{
+		buf[f] = 0;
+		f++;
+		info->cmd_buf_type = CMD_OR;
+	}
+	else if (buf[f] == '&' && buf[f + 1] == '&')
+	{
+		buf[f] = 0;
+		f++;
+		info->cmd_buf_type = CMD_AND;
+	}
+	else if (buf[f] == ';') /* else condition */
+	{
+		buf[f] = 0;
+		info->cmd_buf_type = CMD_CHAIN;
+	}
+	else
+		return (0);
+	*p = f;
+	return (1);
+}
+
+/**
+ * check_chain - main function
+ * @info: input struct
+ * @buf: second input
+ *
+ * @p: input 3 address
+ * @i: input 4
+ * @len: input 5
+ * Return: Always 0
+ */
+
+void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
+{
+	size_t m = *p; /*intialization*/
+
+	if (info->cmd_buf_type == CMD_AND)
+	{
+		if (info->status)
+		{
+			buf[i] = 0;
+			m = len;
+		}
+	}
+	if (info->cmd_buf_type == CMD_OR)
+	{
+		if (!info->status)
+		{
+			buf[i] = 0;
+			m = len;
+		}
+	}
+
+	*p = m;
+}
+
+/**
+ * replace_alias - main function
+ * @info: input
+ * Return: Always 0
+ */
+
+int replace_alias(info_t *info)
+{
+	int x;/*declaration*/
+	list_t *nd; /*declaration*/
+	char *y;/*declaration*/
+
+	for (x = 0; x < 10; x++)
+	{
+		nd = node_starts_with(info->alias, info->argv[0], '=');
+		if (!nd)
+			return (0);
+		free(info->argv[0]);
+		y = _strchr(nd->str, '=');
+		if (!y)
+			return (0);
+		y = _strdup(y + 1);
+		if (!y)
+			return (0);
+		info->argv[0] = y;
+	}
+	return (1);
+}
+
+/**
+ * replace_vars - program start
+ * @info: input main
+ *
+ * Return: Always 0
+ */
+
+int replace_vars(info_t *info)
+{
+	int c = 0;/*declaration*/
+	list_t *nd;/*declaration*/
+
+	for (c = 0; info->argv[c]; c++)
+	{
+		if (info->argv[c][0] != '$' || !info->argv[c][1])
+			continue;
+
+		if (!_strcmp(info->argv[c], "$?"))
+		{
+			replace_string(&(info->argv[c]),
+					_strdup(convert_number(info->status, 10, 0)));
+			continue;
+		}
+		if (!_strcmp(info->argv[c], "$$"))
+		{
+			replace_string(&(info->argv[c]),
+					_strdup(convert_number(getpid(), 10, 0)));
+			continue;
+		}
+		nd = node_starts_with(info->env, &info->argv[c][1], '=');
+		if (nd)
+		{
+			replace_string(&(info->argv[c]),
+					_strdup(_strchr(node->str, '=') + 1));
+			continue;
+		}
+		replace_string(&info->argv[c], _strdup(""));
+
+	}
+	return (0);
+}
+
+/**
+ * replace_string - beginning of program
+ * @old: previous string
+ * @new: new string
+ * Return: Always 0
+ */
+
+int replace_string(char **old, char *new)
+{
+	free(*old);/*freeing*/
+	*old = new;
+	return (1);
+}
